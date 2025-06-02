b5b86baf2413fe2a025edee685b901eeb0ddb192
webfelipemaia <webfelipemaia@gmail.com>
2025-06-02 15:52:51 -0300

    feat: Added application information to the AboutView component

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,19 @@
  
  <template>
-    <div>
-        <p>About</p>
+    <div class="container">
+        <div class="row gx-2">
+            <div class="col extract-wrapper_col p-5 mt-3">
+                <h4 class="pb-5">Sobre</h4>
+                <p><strong>Vue Prepare Image</strong> é uma aplicação web desenvolvida com Vue.js 3, criada para manipulação de imagens para otimizar 
+            o reconhecimento de texto por modelos de Inteligência Artificial (IA). Você pode usar para editar imagens e trabalhar com arquivos PDF para
+            outras atividades, como prefeir. A ferramenta oferece uma interface intuitiva que permite aplicar filtros manuais e ajustes visuais, preparando 
+            as imagens para processos como OCR (Reconhecimento Óptico de Caracteres).
+                </p>
+                <p><small>Versão [VITE_APP_VERSION]</small></p>
+            </div>
+        </div>
     </div>
  </template>
    
  <script setup>
  </script>
- 
